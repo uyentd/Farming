e44a22a0d56bd875a03d325306f3af2bc377e562
uyen <dieu.uyen2305@gmail.com>
2023-09-10 23:34:43 +0700

    limit vị trí camera, input  vị trí camera từ màn hình

---
+++ Assets/Farming/Script/Ingame/CameraControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform Limit_left;
+    public Transform Limit_right;
+    public Transform trans_cam;
+    private float pos_x;
+    public float sensity = 1;
+    public float speed;
+    private void Awake()
+    {
+        //GameManager.instance.ShowMess();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       //trans_cam = transform;
+    }
+
+    void Update()
+    {
+
+    }
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        //Debug.LogError(" đã chạy vào late update");
+        //trans.position = player.position;
+        /* Vector3 delta_move = InputManager.delta_mouse;
+         pos_x = trans_cam.localPosition.x;
+         pos_x = Mathf.Lerp(pos_x, pos_x - delta_move.x * sensity, Time.deltaTime * speed);
+         pos_x = Mathf.Clamp(pos_x, Limit_left.localPosition.x, Limit_right.localPosition.x);
+         trans_cam.localPosition = new Vector3(pos_x ,trans_cam.localPosition.y, trans_cam.localPosition.z);*/
+        //Debug.LogError("đã chạy hết late update"); 
+
+        Vector3 delta_move = InputManager.delta_mouse;
+        pos_x = trans_cam.localPosition.x;
+        pos_x = Mathf.Lerp(pos_x, pos_x - delta_move.x * sensity, Time.deltaTime * speed); // set speed và sensity của input
+        pos_x = Mathf.Clamp(pos_x, Limit_left.localPosition.x, Limit_right.localPosition.x);// set limit 2 đầu
+        trans_cam.localPosition = new Vector3(pos_x, trans_cam.localPosition.y, trans_cam.localPosition.z);
+    }
+
+    // funtion thay đổi vị trí mới của camera theo newtaget
+    public void ChangeTaget( Transform new_taget) // sẽ có 1 hoặc nhiều tham số, hoặc ko có 
+    {
+       // player = new_taget;
+    }
+}
---
+++ Assets/Farming/Script/Ingame/InputManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : BYSingletonMono<InputManager>
+{
+    public static Vector3 delta_mouse; // dùng chuột 
+    private Vector3 ogrinal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* bài trước
+          float x = Input.GetAxis("Horizontal");// 0->1-1
+          float z = Input.GetAxis("Vertical");
+          move_dir = new Vector3(x, 0, z);
+          if (Input.GetKeyDown(KeyCode.K))
+          {
+              isAttack = true;
+          }
+          else
+          {
+              isAttack = false;
+          }
+
+          isSwapChar = Input.GetKeyDown(KeyCode.Space);
+        */
+
+        // dùng chuột kéo thả character
+
+        delta_mouse = Vector3.zero; // gán deltamouse = 0
+      if (Input.GetMouseButtonDown(0))// chuột trái, nhấn
+        {
+            ogrinal = Input.mousePosition;
+        }
+      else if(Input.GetMouseButton(0)) // nhấn trượt thả
+        { 
+            delta_mouse = Input.mousePosition - ogrinal;
+            ogrinal = Input.mousePosition;
+        }
+      else  //thả
+        {
+            delta_mouse = Vector3.zero;
+            ogrinal = Vector3.zero;
+        }
+
+    }
+}
